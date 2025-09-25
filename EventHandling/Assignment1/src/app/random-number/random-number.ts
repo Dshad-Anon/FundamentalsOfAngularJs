56c0cf96e33e7e6ce9299fb1fa7ea0e992dde372
shiva <shiva@DSHAD>
2025-09-25 23:25:36 +0300

    Assignment 2 addition in exisiting assignment 1 file.

--- EventHandling/Assignment1/src/app/random-number/random-number.ts
+++ EventHandling/Assignment1/src/app/random-number/random-number.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-random-number',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './random-number.html',
   styleUrl: './random-number.css'
 })
 export class RandomNumber {
-  randomNumber  = 0;
+  result = 0;
+  randomNumber = 0;
+  inputNumber = 0;
   generateRandomNumber(){
-    this.randomNumber = Math.floor(Math.random()*100)+1;
+    this.randomNumber = Math.floor(Math.random()*100)+ 1;
+    this.result = this.randomNumber + this.inputNumber;
   }
 }
