697bb644c3123bbd6547208e81fc685c57f16a48
shiva <shiva@DSHAD>
2025-09-26 22:17:46 +0300

    Directives Assignment1

---
+++ Directives/Assignment1/src/app/text-color.ts
@@ -0,0 +1,13 @@
+import { Directive, ElementRef, Input, OnChanges, Renderer2, SimpleChanges } from '@angular/core';
+
+@Directive({
+  selector: '[appTextColor]',
+  standalone: true
+})
+export class TextColor implements OnChanges {
+  @Input('appTextColor') color:string = 'black';
+  constructor(private el: ElementRef, private renderer: Renderer2) { }
+  ngOnChanges(changes: SimpleChanges):void {
+    this.renderer.setStyle(this.el.nativeElement, 'color', this.color);
+}
+}
