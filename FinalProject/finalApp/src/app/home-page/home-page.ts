4f9b227fc07225022dd20d4a6ea472d4a65eaada
shiva <shiva@DSHAD>
2025-09-28 10:50:53 +0300

    home page changes

--- FinalProject/finalApp/src/app/home-page/home-page.ts
+++ FinalProject/finalApp/src/app/home-page/home-page.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
-  imports: [FormsModule,CommonModule],
+  imports: [FormsModule,CommonModule,RouterOutlet,RouterModule],
   standalone: true,
   templateUrl: './home-page.html',
   styleUrl: './home-page.css'
